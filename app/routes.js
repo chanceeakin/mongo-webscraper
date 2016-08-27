b0029d88668f7fe41b311d13dd4821471028c34d
Chance <chance@stream.me>
2016-08-26 20:54:14 -0500

    article.js throwing validator error

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,55 @@
-module.exports = function(app) {
+var Note = require('../models/Note.js');
+var Article = require('../models/Article.js');
+var cheerio = require('cheerio');
+var request = require('request');
+
+module.exports = function (app) {
 	console.log('routes module loaded.');
 
-    app.get('/', function(req, res) {
-    	console.log('serving index.hbs');
-    	var hbsObj = {};
-        res.render('index', hbsObj);
-    })
+	app.get('/scrape', function (req, res) {
+		// first, we grab the body of the html with request
+		request('http://www.theonion.com/', function (error, response, html) {
+			// then, we load that into cheerio and save it to $ for a shorthand selector
+			var $ = cheerio.load(html);
+			// now, we grab every h2 within an article tag, and do the following:
+			$('.inner').each(function (i, element) {
 
+				// save an empty result object
+				var result = {};
+
+				// add the text and href of every link, 
+				// and save them as properties of the result obj
+				result.title = $(this).children('header .headline a').text();
+				result.link = $(this).children('header .headline a').attr('href');
+				result.desc = $(this).children('.desc').text();
+
+				// using our Article model, create a new entry.
+				// Notice the (result):
+				// This effectively passes the result object to the entry (and the title and link)
+				var entry = new Article(result);
+
+				// now, save that entry to the db
+				entry.save(function (err, doc) {
+					// log any errors
+					if (err) {
+						console.log(err);
+					}
+					// or log the doc
+					else {
+						console.log(doc);
+					}
+				});
+
+
+			});
+		});
+		// tell the browser that we finished scraping the text.
+		res.send("Scrape Complete");
+	});
+
+	app.get('/', function (req, res) {
+		console.log('serving index.hbs');
+		var hbsObj = {};
+		res.render('index', hbsObj);
+	});
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const gulp = require('gulp'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	nodemon = require('gulp-nodemon');
 
 gulp.task('hello', function() {
   console.log('Hello Zell');
@@ -12,6 +13,14 @@ gulp.task('sass', function(){
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(gulp.dest('public/css'))
 });
+
+gulp.task('start', function () {
+  nodemon({
+    script: 'index.js'
+  , ext: 'js html'
+  , env: { 'NODE_ENV': 'development' }
+  })
+})
 
 gulp.task('watch', function(){
   gulp.watch('bower_components/materialize/sass/materialize.scss', ['sass']); 
